2124900f401b28840aa7b38a99e8ef76102c12ed
Warner Onstine <warnero@gmail.com>
2009-08-06 13:13:03 -0700

    initial commit

---
+++ GormCouchdbGrailsPlugin.groovy
@@ -0,0 +1,50 @@
+class GormCouchdbGrailsPlugin {
+    // the plugin version
+    def version = "0.1"
+    // the version or versions of Grails the plugin is designed for
+    def grailsVersion = "1.1 > *"
+    // the other plugins this plugin depends on
+    def dependsOn = [:]
+    // resources that are excluded from plugin packaging
+    def pluginExcludes = [
+            "grails-app/views/error.gsp"
+    ]
+
+    // TODO Fill in these fields
+    def author = "Warner Onstine"
+    def authorEmail = ""
+    def title = "GORM-CouchDB plugin"
+    def description = '''\\
+A plugin that emulates the behavior of the GORM-Hibernate plugin against a CouchDB-0.9.x NoSQL database
+'''
+
+    // URL to the plugin's documentation
+    def documentation = "http://grails.org/GormCouchdb+Plugin"
+
+    def doWithSpring = {
+        // TODO Implement runtime spring config (optional)
+    }
+
+    def doWithApplicationContext = { applicationContext ->
+        // TODO Implement post initialization spring config (optional)
+    }
+
+    def doWithWebDescriptor = { xml ->
+        // TODO Implement additions to web.xml (optional)
+    }
+
+    def doWithDynamicMethods = { ctx ->
+        // TODO Implement registering dynamic methods to classes (optional)
+    }
+
+    def onChange = { event ->
+        // TODO Implement code that is executed when any artefact that this plugin is
+        // watching is modified and reloaded. The event contains: event.source,
+        // event.application, event.manager, event.ctx, and event.plugin.
+    }
+
+    def onConfigChange = { event ->
+        // TODO Implement code that is executed when the project configuration changes.
+        // The event is the same as for 'onChange'.
+    }
+}
