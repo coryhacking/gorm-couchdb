8bdc4980aba76768c515144d357966c9a969c146
Cory Hacking <coryh@rigel.local>
2009-10-09 13:42:39 -0600

    Added Apache 2 license and copyright.

--- test/integration/com/clearboxmedia/couchdb/BasicPersistenceMethodTests.groovy
+++ test/integration/com/clearboxmedia/couchdb/BasicPersistenceMethodTests.groovy
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2009 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.clearboxmedia.couchdb
 
 import org.acme.Project
@@ -6,6 +21,10 @@ import org.jcouchdb.document.DesignDocument
 import org.jcouchdb.document.DocumentInfo
 import org.jcouchdb.document.View
 
+/**
+ *
+ * @author Warner Onstine, Cory Hacking
+ */
 public class BasicPersistenceMethodTests extends GroovyTestCase {
 
     static transactional = false
