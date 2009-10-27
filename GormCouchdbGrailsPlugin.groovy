b8533cdc12a7a59c25b74b8e7a942ab329665821
Cory Hacking <coryh@rigel.local>
2009-10-27 10:29:06 -0600

    Updated the plugin version to 0.2.

--- GormCouchdbGrailsPlugin.groovy
+++ GormCouchdbGrailsPlugin.groovy
@@ -25,7 +25,7 @@ import org.springframework.core.io.Resource
 class GormCouchdbGrailsPlugin {
 
     // the plugin version
-    def version = '0.1'
+    def version = '0.2'
 
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = '1.1.1 > *'
